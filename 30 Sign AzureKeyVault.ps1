8111b1223d540889d0f5e3c5f0e8edc86f4392ca
Raimund Andree <r.andree@live.com>
2025-04-06 16:16:29 -0700

    Refactor Azure Key Vault scripts for improved credential handling and role assignments

--- 30 Sign AzureKeyVault.ps1
+++ 30 Sign AzureKeyVault.ps1
@@ -7,43 +7,27 @@ $appName = 'CodeSigningDemoApp'
 
 # Get the previously created Azure AD application registration
 $app = Get-AzADApplication -DisplayName $appName
+
 $appSecret = Get-Content -Path .\Assets\secret.txt
-
 $secureAppSecret = ConvertTo-SecureString -String $appSecret -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential ($app.a, $secureAppSecret)
-
-
+$credential = New-Object System.Management.Automation.PSCredential ($app.AppId, $secureAppSecret)
 
 # Connect to Azure using the service principal credentials
 Connect-AzAccount -ServicePrincipal -Tenant $tenantId -Credential $credential
 
-# Let Azure Key Vault handle the signing process
-$credential = New-Object System.Management.Automation.PSCredential ($appId, $secureSecret)
-
-Get-AzKeyVaultCertificate -VaultName $keyVaultName -Name Summit2025
-Get-AzKeyVaultSecret -VaultName $keyVaultName -Name Summit2025
-
+# Install the AzureSignTool tool if not already installed
 dotnet tool install --global AzureSignTool --version 2.0.17
 
 $scriptFile = Get-Item -Path .\Test.ps1
 
-.\AzureSignTool-x64.exe sign --description 'https://vcsjones.com' `
+.\AzureSignTool.exe sign --description 'https://vcsjones.com' `
     --azure-key-vault-url "https://$keyVaultName.vault.azure.net" `
-    --azure-key-vault-client-id $appId `
+    --azure-key-vault-client-id $app.AppId `
     --azure-key-vault-tenant-id $tenantId `
-    --azure-key-vault-client-secret $secret `
-    --azure-key-vault-certificate Summit2025 `
+    --azure-key-vault-client-secret $appSecret `
+    --azure-key-vault-certificate Summit2025Demo `
     --timestamp-rfc3161 http://timestamp.digicert.com `
     --verbose `
     $scriptFile.FullName
-
-
-Key Vault Crypto User
-Key Vault Certificate User
-Key Vault Reader
-
-
-New-AzKeyVaultRoleAssignment -HsmName bez-hsm -RoleDefinitionName 'Managed Hsm Crypto User' -ObjectId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
-
 
 Get-AuthenticodeSignature -FilePath $scriptFile.FullName
--- 40 Retreiving Keys.ps1
+++ 40 Retreiving Keys.ps1
@@ -1,6 +1,39 @@
+$tenantId = 'a1627b4f-281e-4f8b-bf13-bddc0eb6857e'
+Connect-AzAccount -TenantId $tenantId -AuthScope MicrosoftGraphEndpointResourceId
+
+$keyVaultName = 'Summit2025Demo'
+$resourceGroupName = 'Summit2025'
+$appName = 'CodeSigningDemoApp'
+
+# Get the previously created Azure AD application registration
+$app = Get-AzADApplication -DisplayName $appName
+$servicePrincipal = Get-AzADServicePrincipal -ApplicationId $app.AppId
+
+$keyVault = Get-AzKeyVault -VaultName $keyVaultName -ResourceGroupName $resourceGroupName
+$scope = $keyVault.ResourceId
+
+$appSecret = Get-Content -Path .\Assets\secret.txt
+$secureAppSecret = ConvertTo-SecureString -String $appSecret -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($app.AppId, $secureAppSecret)
+
+# Connect to Azure using the service principal credentials
+Connect-AzAccount -ServicePrincipal -Tenant $tenantId -Credential $credential
+
+
 # If this work's, this is bad.
-$certificatePassword = ConvertTo-SecureString -String 'Password1' -AsPlainText -Force
 $secret = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name Summit2025Demo -AsPlainText
 $secretBytes = [System.Convert]::FromBase64String($secret)
 $pfxCertObject = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList $secretBytes, '', 'Exportable'
 [System.IO.File]::WriteAllBytes('.\temp.pfx', $secretBytes)
+
+
+Connect-AzAccount -TenantId $tenantId -AuthScope MicrosoftGraphEndpointResourceId
+$roles = 'Key Vault Crypto User', 'Key Vault Certificate User', 'Key Vault Reader'
+
+Remove-AzRoleAssignment -ObjectId $servicePrincipal.Id -RoleDefinitionName 'Key Vault Certificate User' -Scope $scope | Out-Null
+
+# Test it again with the removed role assignment
+# If this work's, this is bad.
+Connect-AzAccount -ServicePrincipal -Tenant $tenantId -Credential $credential
+
+$secret = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name Summit2025Demo -AsPlainText
